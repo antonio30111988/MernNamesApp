22170d65d76334a03f31a0e164e0144cca432c0e
Antonio <antonio.conipo@gmail.com>
2017-07-17 09:46:41 +0200

    ExoressJS server setup and routing

---
+++ api/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const router = express.Router();
+//API CALL
+router.get('/',(req,res)=>{
+	//send json as its api
+	res.send({data:[]}); 
+});
+export default router;
